81b97df7f407937d43b6fd0c255ab35b165546e7
v.valot <valot.vince@gmail.com>
2015-01-20 09:31:31 +0100

    Modification du protocole

--- protocole.h
+++ protocole.h
@@ -1,26 +1,18 @@
 #define MAX_NAME 20
-#define MAX_CLIENT 50
 #define MAX_MESSAGE 250
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-<<<<<<< HEAD
 typedef struct Client {
     char pseudo[MAX_NAME];
     struct sockaddr_in socket_addr;
 } Client;
-=======
-struct Salon{
-    Char name[MAX_NAME];
+
+typedef struct Salon{
+    char name[MAX_NAME];
     Client * clients;
 };
->>>>>>> d021c13641e77ab2c3f2032b24f65efcd116852b
-
-typedef struct Salon {
-    char nom[MAX_NAME];
-    Client clients[MAX_CLIENT];
-} Salon;
 
 typedef struct Message{
     char message[MAX_MESSAGE];
